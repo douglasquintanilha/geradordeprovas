7be528c2dd770909793f1daeac1189e084e0db48
Douglas Quintanilha <dougquintanilha@gmail.com>
2015-08-17 14:28:07 -0300

    Adicionado o Configurator da SpringFramework

---
+++ src/main/java/com/caelum/geradordeprovas/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.caelum.geradordeprovas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloWorldController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String sayHello() {
+		return "index";
+	}
+}
