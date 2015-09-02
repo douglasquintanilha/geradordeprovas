514ff3f7d27d9353e52dbb3473b6069a3d1b6e11
Douglas Quintanilha <dougquintanilha@gmail.com>
2015-09-02 18:57:44 -0300

    Old input das questoes com Tag funcionando
    Former-commit-id: a4fde014ee02005616937cd22116649890c79c4a

--- src/main/java/com/caelum/geradordeprovas/controllers/TagController.java
+++ src/main/java/com/caelum/geradordeprovas/controllers/TagController.java
@@ -3,6 +3,7 @@ package com.caelum.geradordeprovas.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
