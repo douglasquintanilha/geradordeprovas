25fa201c7fa781d47c00c5bd94c318d9b4906b7d
Douglas Quintanilha <dougquintanilha@gmail.com>
2015-12-11 18:33:40 -0200

    Corrigindo os testes faltando

--- src/test/java/br/com/caelum/geradordeprovas/test/LiberaProvasTest.java
+++ src/test/java/br/com/caelum/geradordeprovas/test/LiberaProvasTest.java
@@ -5,17 +5,34 @@ import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import br.com.caelum.geradordeprovas.configuration.JpaConfigurator;
+import br.com.caelum.geradordeprovas.configuration.TestConfigurator;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {JpaConfigurator.class,TestConfigurator.class})
+@ActiveProfiles("test")
 public class LiberaProvasTest {
 
 	private FirefoxDriver driver;
+
+	@Autowired
+	@Qualifier("rootAplicacao")
+	private String root;
 
 	@Before
 	public void inicializa() {
 		this.driver = new FirefoxDriver();
 		LoginPage pagina = new LoginPage(driver);
-		pagina.visita();
+		pagina.visita(root);
 		String login = "admin";
 		String senha = "12345";
 		pagina.preenche(login, senha);
