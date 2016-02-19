2f302b6223bbb32b75311676d2f372e08b346bc3
silviomm <silviommattos@gmail.com>
2016-02-19 15:54:09 -0200

    configurando ambiente de dev/producao

--- src/main/java/br/com/caelum/geradordeprovas/configuration/ApplicationExceptionHandler.java
+++ src/main/java/br/com/caelum/geradordeprovas/configuration/ApplicationExceptionHandler.java
@@ -1,19 +1,36 @@
 package br.com.caelum.geradordeprovas.configuration;
 
+import java.io.InputStream;
+
+import javax.persistence.NoResultException;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
-//	@ExceptionHandler(Exception.class)
-//	public ModelAndView exception(){
-//		return new ModelAndView("erro");
-//	}
-//	
-//	@ExceptionHandler(NoResultException.class)
-//	public ModelAndView exceptionPersistence(){
-//		return new ModelAndView("erro");
-//	}
+	private InputStream devEnviroment = getClass().getClassLoader()
+			.getResourceAsStream("devEnviroment.properties");
 	
+	private String client_error_jsp = "erro";
+	private String dev_error_jsp = "devError";
 	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView exception(Exception exception) {
+		if (devEnviroment != null)
+			return new ModelAndView(dev_error_jsp).addObject("exception", exception);
+		else
+			return new ModelAndView(client_error_jsp);
+	}
+
+	@ExceptionHandler(NoResultException.class)
+	public ModelAndView exceptionPersistence(Exception exception) {
+		if (devEnviroment != null)
+			return new ModelAndView(dev_error_jsp).addObject("exception", exception);
+		else
+			return new ModelAndView(client_error_jsp);
+	}
+
 }
