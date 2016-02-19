2983c0177a82ca422573c350933f68d4790784a6
Douglas Quintanilha <dougquintanilha@gmail.com>
2016-02-19 16:16:47 -0200

    Teste deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -ev
+
 git init
 git status
 git config --global push.default simple
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
-git checkout -b master
+git checkout -b development
 git add --all
 git commit -am "Travis deploy"
