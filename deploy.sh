2fcb6ebb6caf6087a90a11c0b18fdf7763d96ba0
Douglas Quintanilha <dougquintanilha@gmail.com>
2016-02-19 16:19:33 -0200

    Modificando script de deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git status
 git config --global push.default simple
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
-git checkout -b development
+git checkout -b master
 git add --all
 git commit -am "Travis deploy"
