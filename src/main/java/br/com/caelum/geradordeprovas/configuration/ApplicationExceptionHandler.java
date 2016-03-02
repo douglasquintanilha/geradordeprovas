ad8fb8417d3aa61b380cd6958f38d0885fe62143
silviomm <silviommattos@gmail.com>
2016-03-02 16:53:54 -0300

    stacktrace de erro funcionando

--- src/main/java/br/com/caelum/geradordeprovas/configuration/ApplicationExceptionHandler.java
+++ src/main/java/br/com/caelum/geradordeprovas/configuration/ApplicationExceptionHandler.java
@@ -11,22 +11,23 @@ public class ApplicationExceptionHandler {
 
 	private String environment = System.getenv("ENVIRONMENT");
 
-	
 	private String client_error_jsp = "erro";
 	private String dev_error_jsp = "devError";
-	
+
 	@ExceptionHandler(Exception.class)
 	public ModelAndView exception(Exception exception) {
-		if (environment.equals("local") || environment.equals("dev"))
-			return new ModelAndView(dev_error_jsp).addObject("exception", exception);
-		else
+		if (environment.equals("local") || environment.equals("dev")) {
+			return new ModelAndView(dev_error_jsp).addObject("exception",
+					exception);
+		} else
 			return new ModelAndView(client_error_jsp);
 	}
 
 	@ExceptionHandler(NoResultException.class)
 	public ModelAndView exceptionPersistence(Exception exception) {
 		if (environment.equals("local") || environment.equals("dev"))
-			return new ModelAndView(dev_error_jsp).addObject("exception", exception);
+			return new ModelAndView(dev_error_jsp).addObject("exception",
+					exception);
 		else
 			return new ModelAndView(client_error_jsp);
 	}
